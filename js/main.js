e39136a53d9dcc282322b2761263c3a1b040bab6
ViktoriaGritsenko <olga_zhilinskaya1964@mail.ru>
2016-02-03 22:28:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,59 +20,60 @@ require(['data','deleteTable','createTable','totalSort', 'filter','jQuery'], fun
 		}
 	});
 
-		var pageTwo = document.getElementById('second');
-		pageTwo.addEventListener('click',function (e) {
+	var pageTwo = document.getElementById('second');
+	pageTwo.addEventListener('click',function (e) {
 		if (e.target) {
 			deleteTable(newTable);
 			createTable(books, param, param*2);
-			}
-		});
+		}
+	});	
 	
-		var pageThree = document.getElementById('third');
-		pageThree.addEventListener('click',function (e) {
+	var pageThree = document.getElementById('third');
+	pageThree.addEventListener('click',function (e) {
 		if (e.target) {
 			deleteTable(newTable);
 			createTable(books, 2*param, param*3);
-			}
-		});
+		}
+	});	
+	
 
-		//pagination forward
-		var pageForward = document.getElementById('forwardMod');	
-		pageForward.addEventListener('click',function (e) {
-			if (e.target) {
-					field = document.getElementsByTagName('tr')[1]; 
-					var author = field.getElementsByTagName('td')[0].innerText;
-					var title = field.getElementsByTagName('td')[1].innerText;
-					for (var i=0; i<books.length; i++){		
-						if(books[i]['author'] == author && books[i]['title'] ==title ){
-							deleteTable(newTable);	
-							if ((i+param)<books.length){
-								createTable(books, i+param, i+2*param);		
-							}
-							else createTable(books, i, books.length);		
-						}	
+	//pagination forward
+	var pageForward = document.getElementById('forwardMod');	
+	pageForward.addEventListener('click',function (e) {
+		if (e.target) {
+			field = document.getElementsByTagName('tr')[1]; 
+			var author = field.getElementsByTagName('td')[0].innerText;
+			var title = field.getElementsByTagName('td')[1].innerText;
+			for (var i=0; i<books.length; i++){		
+				if(books[i]['author'] == author && books[i]['title'] ==title ){
+					deleteTable(newTable);	
+					if ((i+param)<books.length){
+						createTable(books, i+param, i+2*param);		
 					}
-				}
-		});
-		
-		//pagination backward
-		var pageBackward = document.getElementById('backwardMod');
-		pageBackward.addEventListener('click',function (e) {
-			if (e.target) {
-				field = document.getElementsByTagName('tr')[1]; 
-				var author = field.getElementsByTagName('td')[0].innerText;
-				var title = field.getElementsByTagName('td')[1].innerText;
-				for (var i=0; i<books.length; i++){
-					if(books[i]['author'] == author && books[i]['title'] ==title ){
-						deleteTable(newTable);				
-						if (i > 0){
-							createTable(books, i-param, i);									
-						}
-						else createTable(books, 0, 5);	
-					}
+					else createTable(books, i, books.length);		
 				}	
 			}
-		});
+		}
+	});		
+		
+	//pagination backward
+	var pageBackward = document.getElementById('backwardMod');
+	pageBackward.addEventListener('click',function (e) {
+		if (e.target) {
+			field = document.getElementsByTagName('tr')[1]; 
+			var author = field.getElementsByTagName('td')[0].innerText;
+			var title = field.getElementsByTagName('td')[1].innerText;
+			for (var i=0; i<books.length; i++){
+				if(books[i]['author'] == author && books[i]['title'] ==title ){
+					deleteTable(newTable);				
+					if (i > 0){
+						createTable(books, i-param, i);									
+					}
+					else createTable(books, 0, 5);	
+				}
+			}	
+		}
+	});
 	
 	//	filter by title (module "filter")
 	var filterParameterButton = document.getElementById('submit');
